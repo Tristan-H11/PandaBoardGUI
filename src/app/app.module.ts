8495a139b2d615df59c8d2bed3cdb006215ac958
Tristan <triddel02@gmx.de>
2021-10-19 10:17:44 +0200

    Commit for gH Pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,22 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { CreateTaskComponent } from './createTask/createTask.component';
+import {AppComponent} from './app.component';
+import {CreateTaskComponent} from './createTask/createTask.component';
 import {ReactiveFormsModule} from "@angular/forms";
 import {HttpClientModule} from "@angular/common/http";
-import { KanbanboardComponent } from './kanbanboard/kanbanboard.component';
+import {KanbanboardComponent} from './kanbanboard/kanbanboard.component';
 import {DragDropModule} from "@angular/cdk/drag-drop";
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatCardModule} from "@angular/material/card";
 import {MatInputModule} from "@angular/material/input";
 import {MatIconModule} from "@angular/material/icon";
 import {MatButtonModule} from "@angular/material/button";
+import {MatSelectModule} from "@angular/material/select";
+import {MatOptionModule} from "@angular/material/core";
+import {MatSnackBar} from "@angular/material/snack-bar";
+import {MatSidenavModule} from "@angular/material/sidenav";
+import {MatTabsModule} from "@angular/material/tabs";
 
 @NgModule({
   declarations: [
@@ -19,18 +24,25 @@ import {MatButtonModule} from "@angular/material/button";
     CreateTaskComponent,
     KanbanboardComponent
   ],
-  imports: [
-    BrowserModule,
-    ReactiveFormsModule,
-    HttpClientModule,
-    DragDropModule,
-    BrowserAnimationsModule,
-    MatCardModule,
-    MatInputModule,
-    MatIconModule,
-    MatButtonModule
+    imports: [
+        BrowserModule,
+        ReactiveFormsModule,
+        HttpClientModule,
+        DragDropModule,
+        BrowserAnimationsModule,
+        MatCardModule,
+        MatInputModule,
+        MatIconModule,
+        MatButtonModule,
+        MatSelectModule,
+        MatOptionModule,
+        MatSidenavModule,
+        MatTabsModule
+    ],
+  providers: [
+    MatSnackBar
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
